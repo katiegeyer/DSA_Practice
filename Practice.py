4debb86128088dd9504b7c47be4070f6cf04854a
Katie Geyer <keg233@nau.edu>
2024-01-22 12:54:05 -0700

    roman to integer

--- Practice.py
+++ Practice.py
@@ -97,7 +97,32 @@ class Solution(object):
 
         return max_length
 
-# Example usage
+# Example 
 solution = Solution()
 example_string = "abcabcbb"
 print(solution.lengthOfLongestSubstring(example_string))  # Expected output: 3
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            value = roman_values[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+
+        return total
+
+# Example
+solution = Solution()
+example_roman = "XXVII"
+print(solution.romanToInt(example_roman))  # Expected output: 27
