6453aaa6950577f590bb3eab71faffe43f5582e7
Katie Geyer <keg233@nau.edu>
2024-01-25 18:09:01 -0700

    merge 2 lists

--- Practice.py
+++ Practice.py
@@ -153,3 +153,37 @@ class Solution(object):
         return prefix
 
 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        # Initialize a dummy node
+        dummy = ListNode()
+        current = dummy
+
+        # Traverse through both lists
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        # Append the remaining elements of list1 or list2
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
+
+        # Return the head of the merged list
+        return dummy.next
