861d92b793281506344b89f6f4174e08ec128e05
Katie Geyer <keg233@nau.edu>
2024-02-01 07:57:41 -0700

    arcpy workshop

---
+++ ArcPy.py
@@ -0,0 +1,82 @@
+# Workshop starter practice problems
+x = 20
+print(x)
+
+############
+
+x = 20 + 3
+print(x)
+
+############
+
+x = 20
+y = 3
+z = x + y
+print(x)
+print(y)
+print(z)
+
+############ bug, data types (2.7 only)
+
+x = 20
+y = 3
+print(x)
+print(y)
+print(x / y)
+
+############
+
+x = 20.0
+y = 3.0
+print(x)
+print(y)
+print(x / y)
+
+############
+
+x = 20.0
+y = 3.0
+z = 5.0
+print(x * y - z)
+print(x + y / z)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+# 55
+# 4.6... but really 20.6
+
+############
+
+h = 'Hello World'
+print(h)
+
+############ bug, no space
+
+h = 'Hello'
+hw = h + 'World'
+print(hw)
+
+############
+
+h = 'Hello'
+hw = h + ' World'
+print(hw)
+
+############
+
+h = 'Hello'
+h += ' World'
+print(h)
+
+############
+
+a = True
+b = False
+print(a)
+print(b)
+
+############
+
+a = True
+b = False
