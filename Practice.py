dcdc0848af385ec28895252c7d54cf525eb3acf4
Katie Geyer <keg233@nau.edu>
2024-02-10 13:00:49 -0700

    3sum

--- Practice.py
+++ Practice.py
@@ -359,3 +359,44 @@ class Solution(object):
                 right -= 1
 
         return max_area
+
+
+def threeSum(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    nums.sort()
+    res = []
+
+    for i in range(len(nums) - 2):
+        # Avoid duplicates for the first number
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        left, right = i + 1, len(nums) - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                res.append([nums[i], nums[left], nums[right]])
+                # Avoid duplicates for the second and third numbers
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+    return res
+
+# Test the function with the given examples
+example1 = threeSum([-1,0,1,2,-1,-4])  # Should return [[-1,-1,2],[-1,0,1]]
+example2 = threeSum([0,1,1])  # Should return []
+example3 = threeSum([0,0,0])  # Should return [[0,0,0]]
+
+example1, example2, example3
