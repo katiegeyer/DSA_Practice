467d91e664c5628c27c7dd179e48442bed0b2aea
Katie Geyer <keg233@nau.edu>
2024-02-22 23:01:07 -0700

    8ball game

--- Practice.py
+++ Practice.py
@@ -576,3 +576,35 @@ def rocket_ship():
 
 
 rocket_ship()
+
+import random
+
+def magic_8_ball():
+    responses = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes – definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    ]
+
+    input("What is your question? ")
+    print("Thinking...")
+    print(random.choice(responses))
+
+magic_8_ball()
