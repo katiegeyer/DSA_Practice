a69cde473f38f9d913f98c28832cfb267bbdc24d
Katie Geyer <keg233@nau.edu>
2024-03-11 12:10:12 -0700

    listnode reverse numbers tech interview pairboarding;

--- PairBoardingProbs.js
+++ PairBoardingProbs.js
@@ -17,4 +17,28 @@ const uniqueNumbers = removeDuplicates(numbers);
 console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
 
 
+//commented tech interview pairboarding, listnode helper and reverse list
 
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+function reverseList(head) {
+    let prev = null;
+    let current = head;
+    let next = null;
+
+    while (current != null) {
+        // Store the next node
+        next = current.next;
+        // Reverse the current node's pointer
+        current.next = prev;
+        // Move pointers one position ahead
+        prev = current;
+        current = next;
+    }
+    head = prev;
+
+    return head;
+}
