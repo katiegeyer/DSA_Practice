dc926e51c8af621df95c5bc560a0b4a02a73a433
Katie Geyer <keg233@nau.edu>
2024-03-30 20:26:59 -0700

    validating binary tree

--- TechInterview.js
+++ TechInterview.js
@@ -12,4 +12,20 @@ function mergeIntervals(intervals) {
 }
 
 // Example usage
-console.log(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]));  // Output: [[1,6],[8,10],[15,18]]
+console.log(mergeIntervals([[1, 3], [2, 6], [8, 10], [15, 18]]));  // Output: [[1,6],[8,10],[15,18]]
+
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function isValidBST(root, low = -Infinity, high = Infinity) {
+    if (!root) return true;
+    if (root.val <= low || root.val >= high) return false;
+    return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, high);
+}
+
+// Example requires building a binary tree
