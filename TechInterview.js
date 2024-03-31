7b97c5127c460b493e28ec648f50667952221cac
Katie Geyer <keg233@nau.edu>
2024-03-30 20:27:58 -0700

    merge

--- TechInterview.js
+++ TechInterview.js
@@ -50,3 +50,18 @@ function reverseLinkedList(head) {
 }
 
 // Example need to create a linked list
+
+function twoSum(nums, target) {
+    const numMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in numMap) {
+            return [numMap[complement], i];
+        }
+        numMap[nums[i]] = i;
+    }
+    return [];
+}
+
+// Example usage
+console.log(twoSum([2, 7, 11, 15], 9));  // Output: [0, 1]
