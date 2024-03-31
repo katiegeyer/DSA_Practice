ec1a1059e6dd5230a6c50c94fa98b8b8f672560a
Katie Geyer <keg233@nau.edu>
2024-03-30 20:31:04 -0700

    merge

--- TechInterview.py
+++ TechInterview.py
@@ -65,3 +65,15 @@ def reverse_linked_list(head):
     return prev
 
 # Example create a linked list
+
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+
+# Example usage
+print(two_sum([2, 7, 11, 15], 9))  # Output: [0, 1]
