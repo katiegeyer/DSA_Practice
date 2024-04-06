79a49b099a00cedac8ea0754a74d0125a4f1cc81
Katie Geyer <keg233@nau.edu>
2024-04-06 13:40:52 -0700

    fizzbuzz

--- Practice.py
+++ Practice.py
@@ -886,3 +886,13 @@ original_string = "Hello, world!"
 reversed_string = reverse_string(original_string)
 print(f"Original: {original_string}")
 print(f"Reversed: {reversed_string}")
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
