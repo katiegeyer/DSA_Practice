db873eab5edf50a0fd2b372d1773c01bfc834d44
Katie Geyer <keg233@nau.edu>
2024-05-04 20:20:13 -0700

    peak element

--- Practice.js
+++ Practice.js
@@ -504,3 +504,19 @@ function pancakeSort(stack) {
 const stack = [3, 1, 4, 2, 5];
 const sortedStack = pancakeSort(stack);
 console.log("Sorted Stack:", sortedStack);
+
+def find_peak_element(nums):
+left, right = 0, len(nums) - 1
+
+while left < right:
+    mid = (left + right) // 2
+if nums[mid] > nums[mid + 1]:
+    right = mid
+else:
+left = mid + 1
+
+return left
+
+# Example usage:
+nums = [1, 2, 1, 3, 5, 6, 4]
+print("Peak Element Index:", find_peak_element(nums))
