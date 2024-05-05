992528e3ee766a527610a898c3cdc6ab3d89e56a
Katie Geyer <keg233@nau.edu>
2024-05-04 20:12:44 -0700

    peak element

--- Practice.js
+++ Practice.js
@@ -375,3 +375,23 @@ function generateSubsets(set) {
 // Example usage:
 const mySet = [1, 2, 3];
 console.log(generateSubsets(mySet));
+
+function findPeakElement(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+// Example usage:
+const nums = [1, 2, 1, 3, 5, 6, 4];
+console.log("Peak Element Index:", findPeakElement(nums));
