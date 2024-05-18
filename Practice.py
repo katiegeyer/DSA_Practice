2d09f966c5a6a6d3c3467a6f45f558bdb07f253e
Katie Geyer <keg233@nau.edu>
2024-05-18 10:03:40 -0700

    good ol fib

--- Practice.py
+++ Practice.py
@@ -1432,3 +1432,16 @@ nums = [1, 1, 2, 2, 3, 4, 4, 5]
 length = remove_duplicates(nums)
 print(length)  # Output: 5
 print(nums[:length])  # Output: [1, 2, 3, 4, 5]
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+
+# Test
+print(fibonacci(10))  # Output: 55
