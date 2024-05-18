172831455795da592958de99dd6a4f2927ac668a
Katie Geyer <keg233@nau.edu>
2024-05-18 10:03:59 -0700

    is the number prime?

--- Practice.py
+++ Practice.py
@@ -1445,3 +1445,17 @@ def fibonacci(n):
 
 # Test
 print(fibonacci(10))  # Output: 55
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+# Test
+print(is_prime(7))  # Output: True
+print(is_prime(10))  # Output: False
