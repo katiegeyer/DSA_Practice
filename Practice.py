af606775b3766685c218d427668f32131d2bb8ef
Katie Geyer <keg233@nau.edu>
2024-05-18 10:02:24 -0700

    longest palindrome

--- Practice.py
+++ Practice.py
@@ -1388,3 +1388,29 @@ tm.set("foo", "bar", 1)
 print(tm.get("foo", 1))  # Output: "bar"
 # Should return "bar" since timestamp 3 >= 1 and there are no later entries
 print(tm.get("foo", 3))
+
+
+def longest_palindrome(s):
+    if not s or len(s) < 1:
+        return ""
+    start, end = 0, 0
+    for i in range(len(s)):
+        len1 = expand_around_center(s, i, i)
+        len2 = expand_around_center(s, i, i + 1)
+        length = max(len1, len2)
+        if length > end - start:
+            start = i - (length - 1) // 2
+            end = i + length // 2
+    return s[start:end + 1]
+
+
+def expand_around_center(s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1
+
+
+# Test
+print(longest_palindrome("babad"))  # Output: "bab" or "aba"
+print(longest_palindrome("cbbd"))  # Output: "bb"
