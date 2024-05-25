4592d71a0dc5c7804fad6f5788700600611e4440
Katie Geyer <keg233@nau.edu>
2024-05-25 16:38:02 -0700

    fizz buzz

--- Practice.js
+++ Practice.js
@@ -892,3 +892,20 @@ const debouncedFunction = debounce(() => console.log("Executed!"), 2000);
 debouncedFunction();
 debouncedFunction();
 debouncedFunction(); // Only this call will execute the function after 2 seconds.
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// Example usage:
+fizzBuzz();
