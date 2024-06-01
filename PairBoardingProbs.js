ac4b8c25cd4ff2eeb6c908a89350c50929524b60
Katie Geyer <keg233@nau.edu>
2024-06-01 16:38:19 -0700

    throttle function

--- PairBoardingProbs.js
+++ PairBoardingProbs.js
@@ -179,3 +179,15 @@ function deepClone(obj, hash = new WeakMap()) {
         key => ({ [key]: deepClone(obj[key], hash) })
     ));
 }
+
+function throttle(func, limit) {
+    let inThrottle;
+    return function (...args) {
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
