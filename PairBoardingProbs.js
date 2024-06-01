76b372f81680d4d46378f557763d3dfb6825cffd
Katie Geyer <keg233@nau.edu>
2024-06-01 16:38:35 -0700

    event emitter

--- PairBoardingProbs.js
+++ PairBoardingProbs.js
@@ -191,3 +191,31 @@ function throttle(func, limit) {
         }
     };
 }
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(listener);
+    }
+
+    emit(event, ...args) {
+        if (this.events[event]) {
+            this.events[event].forEach(listener => listener.apply(this, args));
+        }
+    }
+
+    off(event, listener) {
+        if (!this.events[event]) return;
+
+        const index = this.events[event].indexOf(listener);
+        if (index > -1) {
+            this.events[event].splice(index, 1);
+        }
+    }
+}
