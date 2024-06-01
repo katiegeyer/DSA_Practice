02f5532a5f9b94bb743341a388e35763324cc3df
Katie Geyer <keg233@nau.edu>
2024-06-01 16:37:41 -0700

    flatten nested array

--- Practice.js
+++ Practice.js
@@ -944,3 +944,9 @@ function customPromiseAll(promises) {
         });
     });
 }
+
+function flattenArray(arr) {
+    return arr.reduce((flat, toFlatten) => {
+        return flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten);
+    }, []);
+}
