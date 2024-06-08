25044b1771dafc31bc5571b950d6848e1d2f4740
Katie Geyer <keg233@nau.edu>
2024-06-08 13:30:40 -0700

    merge python

--- Practice.py
+++ Practice.py
@@ -1504,3 +1504,25 @@ def is_valid(s):
 # Example usage
 print(is_valid("()[]{}"))  # Output: True
 print(is_valid("(]"))  # Output: False
+
+
+def merge(intervals):
+    if not intervals:
+        return []
+
+    intervals.sort(key=lambda x: x[0])
+    merged = [intervals[0]]
+
+    for current in intervals[1:]:
+        last = merged[-1]
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged
+
+
+# Example usage
+# Output: [[1, 6], [8, 10], [15, 18]]
+print(merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
