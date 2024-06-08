fc24d66d39891e89acbc472f68a00548efeaaf8d
Katie Geyer <keg233@nau.edu>
2024-06-08 12:41:40 -0700

    valid paranthesis

--- Practice.js
+++ Practice.js
@@ -1007,3 +1007,28 @@ function lengthOfLongestSubstring(s) {
 
 // Example usage
 console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+
+function isValid(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+        } else {
+            if (stack.pop() !== map[s[i]]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+// Example usage
+console.log(isValid("()[]{}")); // Output: true
+console.log(isValid("(]")); // Output: false
