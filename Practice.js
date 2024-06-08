12f9586278e025a7e624694391f255e07add024f
Katie Geyer <keg233@nau.edu>
2024-06-08 12:34:16 -0700

    two sums

--- Practice.js
+++ Practice.js
@@ -973,3 +973,18 @@ function binarySearch(arr, target) {
 
     return -1;
 }
+
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+// Example usage
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
