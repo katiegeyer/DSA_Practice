826786e07b6d51d4a4f1b40e0c1877e10c02f0b0
Katie Geyer <keg233@nau.edu>
2024-06-15 21:42:39 -0700

    merging lists leetcode

--- Practice.js
+++ Practice.js
@@ -1274,3 +1274,32 @@ function isValid(s) {
 console.log(isValid("()")); // Output: true
 console.log(isValid("()[]{}")); // Output: true
 console.log(isValid("(]")); // Output: false
+
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+function mergeTwoLists(l1, l2) {
+    const dummy = new ListNode(-1);
+    let current = dummy;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val <= l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    current.next = l1 === null ? l2 : l1;
+    return dummy.next;
+}
+
+// Example usage:
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+console.log(mergeTwoLists(list1, list2)); // Output: [1, 1, 2, 3, 4, 4]
