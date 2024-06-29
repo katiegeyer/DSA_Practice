38dc2578de3d746a6e80c41fc3bc9e4a021a1b72
Katie Geyer <keg233@nau.edu>
2024-06-28 23:38:06 -0700

    first missing positive

--- Practice.js
+++ Practice.js
@@ -1412,3 +1412,26 @@ console.log(longestSubstringWithoutRepeatingCharacters("bbbbb"));    // Output: 
 console.log(longestSubstringWithoutRepeatingCharacters("pwwkew"));   // Output: 3
 console.log(longestSubstringWithoutRepeatingCharacters(""));         // Output: 0
 console.log(longestSubstringWithoutRepeatingCharacters("dvdf"));     // Output: 3
+
+function firstMissingPositive(nums) {
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
+
+// Test cases
+console.log(firstMissingPositive([1, 2, 0]));    // Output: 3
+console.log(firstMissingPositive([3, 4, -1, 1])); // Output: 2
+console.log(firstMissingPositive([7, 8, 9, 11, 12])); // Output: 1
