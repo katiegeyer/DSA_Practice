8bf130ecbc2aca863836e14989967912a3548a75
Katie Geyer <keg233@nau.edu>
2024-07-13 13:03:26 -0700

    rotate image

--- Practice.js
+++ Practice.js
@@ -1684,3 +1684,27 @@ function groupAnagrams(strs) {
 // Example usage:
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
 // Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
+
+function rotate(matrix) {
+    const n = matrix.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+}
+
+// Example usage:
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+rotate(matrix);
+console.log(matrix);
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
