60a383929bb7f4dacf30768b00382c0e31bf5400
Katie Geyer <keg233@nau.edu>
2024-07-20 12:44:42 -0700

    kmp

--- Practice.js
+++ Practice.js
@@ -1708,3 +1708,53 @@ const matrix = [
 rotate(matrix);
 console.log(matrix);
 // Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+function computeLPSArray(pattern) {
+    const lps = Array(pattern.length).fill(0);
+    let length = 0;
+    let i = 1;
+
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        } else {
+            if (length !== 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+
+function KMPSearch(text, pattern) {
+    const lps = computeLPSArray(pattern);
+    const result = [];
+    let i = 0; // index for text
+    let j = 0; // index for pattern
+
+    while (i < text.length) {
+        if (pattern[j] === text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j === pattern.length) {
+            result.push(i - j);
+            j = lps[j - 1];
+        } else if (i < text.length && pattern[j] !== text[i]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return result;
+}
+
+console.log(KMPSearch("ababcabcabababd", "ababd")); // Output: [10]
