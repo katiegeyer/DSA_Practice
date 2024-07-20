2352e2d5826faddd752574440facd6d12f482ff0
Katie Geyer <keg233@nau.edu>
2024-07-20 12:35:57 -0700

    kmp search babyyy

--- Practice.py
+++ Practice.py
@@ -1901,3 +1901,47 @@ def groupAnagrams(strs):
 # Example usage:
 print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
 # Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
+
+
+def compute_lps_array(pattern):
+    lps = [0] * len(pattern)
+    length = 0
+    i = 1
+
+    while i < len(pattern):
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
+
+
+def kmp_search(text, pattern):
+    lps = compute_lps_array(pattern)
+    result = []
+    i = 0  # index for text
+    j = 0  # index for pattern
+
+    while i < len(text):
+        if pattern[j] == text[i]:
+            i += 1
+            j += 1
+
+        if j == len(pattern):
+            result.append(i - j)
+            j = lps[j - 1]
+        elif i < len(text) and pattern[j] != text[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    return result
+
+
+print(kmp_search("ababcabcabababd", "ababd"))  # Output: [10]
