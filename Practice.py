e9086d2f972a9fe4538ad3d0e93064fa7504631e
Katie Geyer <keg233@nau.edu>
2024-07-20 12:38:38 -0700

    longest subsequence

--- Practice.py
+++ Practice.py
@@ -2068,3 +2068,20 @@ print(trie.search("app"))     # Output: False
 print(trie.starts_with("app"))  # Output: True
 trie.insert("app")
 print(trie.search("app"))     # Output: True
+
+
+def longest_common_subsequence(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[m][n]
+
+
+print(longest_common_subsequence("ABCBDAB", "BDCAB"))  # Output: 4
