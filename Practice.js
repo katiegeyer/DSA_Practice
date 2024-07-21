dcfa6b76f5223f5218ff571e326ba219e0c53f6d
Katie Geyer <keg233@nau.edu>
2024-07-20 20:41:48 -0700

    prefix tree

--- Practice.js
+++ Practice.js
@@ -1777,3 +1777,57 @@ function longestCommonSubsequence(str1, str2) {
 }
 
 console.log(longestCommonSubsequence("ABCBDAB", "BDCAB")); // Output: 4
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+        for (const char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEndOfWord = true;
+    }
+
+    search(word) {
+        let node = this.root;
+        for (const char of word) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return node.isEndOfWord;
+    }
+
+    startsWith(prefix) {
+        let node = this.root;
+        for (const char of prefix) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return true;
+    }
+}
+
+const trie = new Trie();
+trie.insert("apple");
+console.log(trie.search("apple"));   // Output: true
+console.log(trie.search("app"));     // Output: false
+console.log(trie.startsWith("app")); // Output: true
+trie.insert("app");
+console.log(trie.search("app"));     // Output: true
