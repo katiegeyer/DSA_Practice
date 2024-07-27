fe0abefb83f78b9a1e0427b6958eefe15ff04ea1
Katie Geyer <keg233@nau.edu>
2024-07-27 13:51:52 -0700

    two sum

--- Practice.py
+++ Practice.py
@@ -2176,3 +2176,17 @@ def reverse_string(s):
 s = ['h', 'e', 'l', 'l', 'o']
 reverse_string(s)
 print(s)  # Output: ['o', 'l', 'l', 'e', 'h']
+
+
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+
+
+# Example usage:
+print(two_sum([2, 7, 11, 15], 9))  # Output: [0, 1]
