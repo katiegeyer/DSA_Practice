e00511819498b44ff0d56250434619f1a9637b55
Katie Geyer <keg233@nau.edu>
2024-08-03 16:30:02 -0700

    min

--- Practice.js
+++ Practice.js
@@ -1962,3 +1962,23 @@ function mergeIntervals(intervals) {
 
 // Example usage:
 console.log(mergeIntervals([[1, 3], [2, 6], [8, 10], [15, 18]])); // Output: [[1, 6], [8, 10], [15, 18]]
+
+
+function findMin(nums) {
+    let left = 0, right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+}
+
+// Example usage:
+console.log(findMin([3, 4, 5, 1, 2])); // Output: 1
+
