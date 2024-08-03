a23a089effec119d6995a3fd11230e2293af611d
Katie Geyer <keg233@nau.edu>
2024-08-03 16:30:54 -0700

    exists

--- Practice.js
+++ Practice.js
@@ -1982,3 +1982,35 @@ function findMin(nums) {
 // Example usage:
 console.log(findMin([3, 4, 5, 1, 2])); // Output: 1
 
+function exist(board, word) {
+    function dfs(board, word, i, j, k) {
+        if (k === word.length) return true;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] !== word[k]) return false;
+
+        let tmp = board[i][j];
+        board[i][j] = '/';
+        let res = dfs(board, word, i + 1, j, k + 1) ||
+                  dfs(board, word, i - 1, j, k + 1) ||
+                  dfs(board, word, i, j + 1, k + 1) ||
+                  dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (dfs(board, word, i, j, 0)) return true;
+        }
+    }
+    return false;
+}
+
+// Example usage:
+let board = [
+    ['A','B','C','E'],
+    ['S','F','C','S'],
+    ['A','D','E','E']
+];
+let word = "ABCCED";
+console.log(exist(board, word)); // Output: true
+
