2881e46a0a947cbf29b311f48b2a74cd726bbfe5
Katie Geyer <keg233@nau.edu>
2024-08-03 17:19:54 -0700

    word search

--- Practice.py
+++ Practice.py
@@ -2190,3 +2190,34 @@ def two_sum(nums, target):
 
 # Example usage:
 print(two_sum([2, 7, 11, 15], 9))  # Output: [0, 1]
+
+
+def exist(board: List[List[str]], word: str) -> bool:
+    def dfs(board, word, i, j, k):
+        if k == len(word):
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
+            return False
+        tmp, board[i][j] = board[i][j], '/'
+        res = dfs(board, word, i + 1, j, k + 1) or \
+            dfs(board, word, i - 1, j, k + 1) or \
+            dfs(board, word, i, j + 1, k + 1) or \
+            dfs(board, word, i, j - 1, k + 1)
+        board[i][j] = tmp
+        return res
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if dfs(board, word, i, j, 0):
+                return True
+    return False
+
+
+# Example usage:
+board = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+]
+word = "ABCCED"
+print(exist(board, word))  # Output: True
