340f4528744a4f37480bce24befe844729c89e16
Katie Geyer <118399974+katiegeyer@users.noreply.github.com>
2024-08-18 06:33:13 +0200

    Roman

--- PairBoardingProbs.js
+++ PairBoardingProbs.js
@@ -336,3 +336,35 @@ function lengthOfLongestSubstring(s) {
 // Example usage
 console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3 ("abc")
 console.log(lengthOfLongestSubstring("bbbbb"));    // Output: 1 ("b")
+
+function intToRoman(num) {
+    const romanMap = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ];
+
+    let result = '';
+    
+    for (let [value, symbol] of romanMap) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
+}
+
+// Example usage
+console.log(intToRoman(1987)); // Output: "MCMLXXXVII"
