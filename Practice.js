714e484f542d1f8dd9ec9e6b9e13ce8a7bd32c6a
Katie Geyer <118399974+katiegeyer@users.noreply.github.com>
2024-08-24 14:47:00 +0200

    fizzbuzz

--- Practice.js
+++ Practice.js
@@ -2108,3 +2108,20 @@ function isPrime(num) {
 // Example usage:
 console.log(isPrime(7));  // true
 console.log(isPrime(10)); // false
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// Example usage:
+fizzBuzz(15);
