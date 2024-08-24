062a9b4d5ed0ecd79606b76504df385daa785fe7
Katie Geyer <118399974+katiegeyer@users.noreply.github.com>
2024-08-24 14:47:16 +0200

    wordLength

--- Practice.js
+++ Practice.js
@@ -2125,3 +2125,19 @@ function fizzBuzz(n) {
 
 // Example usage:
 fizzBuzz(15);
+
+function findLongestWord(sentence) {
+    const words = sentence.split(' ');
+    let longestWord = '';
+
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+
+    return longestWord;
+}
+
+// Example usage:
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // "jumped"
