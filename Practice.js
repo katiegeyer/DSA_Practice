4fd001f49d2c4529cd1693c2a4b5484ae2a6d3b6
Katie Geyer <118399974+katiegeyer@users.noreply.github.com>
2024-08-24 14:46:39 +0200

    isthenumprime

--- Practice.js
+++ Practice.js
@@ -2093,3 +2093,18 @@ function reverseString(str) {
 
 // Example usage:
 console.log(reverseString("hello")); // "olleh"
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+// Example usage:
+console.log(isPrime(7));  // true
+console.log(isPrime(10)); // false
