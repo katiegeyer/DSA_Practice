505397dc07992d66eccb4a823f7bd11d5fef024c
Katie Geyer <keg233@nau.edu>
2024-08-31 13:10:22 -0700

    adding dup

--- Practice.js
+++ Practice.js
@@ -2112,3 +2112,9 @@ function fizzBuzz() {
 }
 
 fizzBuzz();
+
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
