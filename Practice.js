bf33d8cb9ee296a3e8906843de1a9f6dfc426aa5
Katie Geyer <keg233@nau.edu>
2024-08-31 13:10:08 -0700

    fizzzbuz

--- Practice.js
+++ Practice.js
@@ -2101,3 +2101,14 @@ function flattenArray(arr) {
 }
 
 console.log(flattenArray([1, [2, [3, [4, 5]]], 6])); // Output: [1, 2, 3, 4, 5, 6]
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
+    }
+}
+
+fizzBuzz();
