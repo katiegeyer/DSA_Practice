3491a1f8c3d42d3a017a77914013b0dd855decfe
Katie Geyer <keg233@nau.edu>
2024-08-31 13:11:12 -0700

    binary search

--- Practice.js
+++ Practice.js
@@ -2124,3 +2124,19 @@ function capitalizeWords(str) {
 }
 
 console.log(capitalizeWords("hello world")); // Output: "Hello World"
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return -1;
+}
+
+console.log(binarySearch([1, 3, 5, 7, 9], 5)); // Output: 2
