a5f8707e2843d60b64e7ffc3572de9ade1fa99ae
Katie Geyer <keg233@nau.edu>
2024-09-07 14:30:29 -0700

    binary search

--- Practice.js
+++ Practice.js
@@ -2215,3 +2215,19 @@ function removeDuplicates(arr) {
 
 // Example
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1; // target not found
+}
+
+// Example
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 5)); // 4
