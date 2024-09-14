cd8de9ece745b13db144024dcd07944f0fc93d79
Katie Geyer <keg233@nau.edu>
2024-09-14 10:29:42 -0700

    permutations

--- Practice.js
+++ Practice.js
@@ -2373,3 +2373,18 @@ function firstNonRepeatingChar(str) {
 }
 
 console.log(firstNonRepeatingChar("swiss")); // Output: "w"
+
+function permute(str) {
+    if (str.length <= 1) return [str];
+    let permutations = [];
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        let remaining = str.slice(0, i) + str.slice(i + 1);
+        for (let perm of permute(remaining)) {
+            permutations.push(char + perm);
+        }
+    }
+    return permutations;
+}
+
+console.log(permute("abc")); // Output: ["abc", "acb", "bac", "bca", "cab", "cba"]
