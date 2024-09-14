e36de357e48b04da9306aeb565eb31084c8299c0
Katie Geyer <keg233@nau.edu>
2024-09-14 10:30:20 -0700

    max subarray

--- Practice.js
+++ Practice.js
@@ -2404,3 +2404,15 @@ function areAnagrams(str1, str2) {
 }
 
 console.log(areAnagrams("listen", "silent")); // Output: true
+
+function maxSubArray(nums) {
+    let maxSoFar = nums[0];
+    let maxEndingHere = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Output: 6
