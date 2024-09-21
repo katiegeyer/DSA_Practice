4ebabdda94231869b1a44f8aac868b26341707c8
Katie Geyer <keg233@nau.edu>
2024-09-21 14:44:25 -0700

    max subarray

--- Practice.py
+++ Practice.py
@@ -2456,3 +2456,11 @@ def merge_intervals(intervals):
         else:
             merged[-1][1] = max(merged[-1][1], interval[1])
     return merged
+
+
+def max_sub_array(nums):
+    current_sum = max_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
