68a297ffc8c9aeb5704f60358b2d6753528e997e
Katie Geyer <keg233@nau.edu>
2024-09-21 14:45:26 -0700

    search

--- Practice.py
+++ Practice.py
@@ -2473,3 +2473,22 @@ def climb_stairs(n):
     for _ in range(3, n+1):
         a, b = b, a + b
     return b
+
+
+def search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
