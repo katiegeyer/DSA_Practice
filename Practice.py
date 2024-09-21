b5fe336c154a05d6d5263f201dc1f2dce30c3646
Katie Geyer <keg233@nau.edu>
2024-09-21 14:42:59 -0700

    longest sub

--- Practice.py
+++ Practice.py
@@ -2421,3 +2421,14 @@ def two_sum(nums, target):
 def is_palindrome(x):
     s = str(x)
     return s == s[::-1]
+
+
+def length_of_longest_substring(s):
+    char_map = {}
+    left = max_length = 0
+    for right, char in enumerate(s):
+        if char in char_map and char_map[char] >= left:
+            left = char_map[char] + 1
+        char_map[char] = right
+        max_length = max(max_length, right - left + 1)
+    return max_length
