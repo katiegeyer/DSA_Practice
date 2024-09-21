a1f958740acfa5c4751b66dfd5ef1f83cff310ec
Katie Geyer <keg233@nau.edu>
2024-09-21 14:43:52 -0700

    intervcals

--- Practice.py
+++ Practice.py
@@ -2445,3 +2445,14 @@ def is_valid(s):
         else:
             stack.append(char)
     return not stack
+
+
+def merge_intervals(intervals):
+    intervals.sort(key=lambda x: x[0])
+    merged = []
+    for interval in intervals:
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
+        else:
+            merged[-1][1] = max(merged[-1][1], interval[1])
+    return merged
