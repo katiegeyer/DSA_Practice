b6c1f22958d99b3682cf44d1ed64316477b2e84b
Katie Geyer <keg233@nau.edu>
2024-09-28 21:43:42 -0700

    fizzbuzz

--- Practice.py
+++ Practice.py
@@ -2554,3 +2554,25 @@ def fibonacci(n):
 
 
 print(fibonacci(10))
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif is_prime(i):
+        print("Prime")
+    else:
+        print(i)
