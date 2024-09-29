14aa40fb8e82867e24430fe2f4cb1289f0107af1
Katie Geyer <keg233@nau.edu>
2024-09-28 21:42:16 -0700

    unique characters

--- Practice.py
+++ Practice.py
@@ -2521,3 +2521,11 @@ board = [
     ['O', 'X', ' ']
 ]
 print(check_winner(board))  # Output: X
+
+
+def has_unique_characters(s):
+    return len(set(s)) == len(s)
+
+
+print(has_unique_characters("abcdef"))
+print(has_unique_characters("hello"))
