dbdd5b91f4f9b1859ef25c0ebda128efe0d690f4
Katie Geyer <keg233@nau.edu>
2024-09-28 21:41:46 -0700

    check winner

--- Practice.py
+++ Practice.py
@@ -2499,3 +2499,25 @@ def mask_data(data):
 
 
 print(mask_data("1234567812345678"))  # Output: ************5678
+
+
+def check_winner(board):
+    # Check rows, columns, and diagonals
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != ' ':
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != ' ':
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return board[0][2]
+    return None
+
+
+board = [
+    ['X', 'O', 'X'],
+    [' ', 'X', 'O'],
+    ['O', 'X', ' ']
+]
+print(check_winner(board))  # Output: X
