ed5e9e9c7bfa7d978d99f2b00b90bc16d34b61aa
Katie Geyer <keg233@nau.edu>
2024-10-05 18:16:57 -0700

    factorial

--- Practice.js
+++ Practice.js
@@ -2448,3 +2448,11 @@ function isPalindrome(str) {
 
 // Example
 console.log(isPalindrome('A man, a plan, a canal, Panama')); // Output: true
+
+function factorial(num) {
+    if (num <= 1) return 1;
+    return num * factorial(num - 1);
+}
+
+// Example
+console.log(factorial(5)); // Output: 120
