b1eb88363bd7ad2d1592227c6f9ae58bb54a1ea3
Katie Geyer <keg233@nau.edu>
2024-10-05 18:20:02 -0700

    merge sorted array

--- Practice.js
+++ Practice.js
@@ -2494,3 +2494,23 @@ function removeDuplicates(arr) {
 
 // Example
 console.log(removeDuplicates([1, 2, 3, 3, 4, 5, 5])); // Output: [1, 2, 3, 4, 5]
+
+function mergeSortedArrays(arr1, arr2) {
+    let merged = [];
+    let i = 0, j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            merged.push(arr1[i]);
+            i++;
+        } else {
+            merged.push(arr2[j]);
+            j++;
+        }
+    }
+
+    return merged.concat(arr1.slice(i)).concat(arr2.slice(j));
+}
+
+// Example
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
