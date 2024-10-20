a07cf39b9d0a7956386c2d42e3c68be69d52ce1f
Katie Geyer <keg233@nau.edu>
2024-10-19 20:33:17 -0700

    fizz buzz

--- Practice.js
+++ Practice.js
@@ -2541,3 +2541,12 @@ function capitalizeWords(str) {
 function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        let output = '';
+        if (i % 3 === 0) output += 'Fizz';
+        if (i % 5 === 0) output += 'Buzz';
+        console.log(output || i);
+    }
+}
