6a6b74cc539855f197897037669f9554c1846d7a
Katie Geyer <keg233@nau.edu>
2024-10-19 20:34:42 -0700

    reverse string

--- Practice.js
+++ Practice.js
@@ -2563,3 +2563,7 @@ function isPalindrome(str) {
     const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     return cleanedStr === cleanedStr.split('').reverse().join('');
 }
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
