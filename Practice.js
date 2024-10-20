3c122b9039be49dc876f0dda9b46dc4fc3b547ae
Katie Geyer <keg233@nau.edu>
2024-10-19 20:32:27 -0700

    sumarray

--- Practice.js
+++ Practice.js
@@ -2528,3 +2528,8 @@ function findMissingNumber(arr, n) {
     const actualSum = arr.reduce((acc, num) => acc + num, 0);
     return expectedSum - actualSum;
 }
+
+function sumArray(arr) {
+    if (arr.length === 0) return 0;
+    return arr[0] + sumArray(arr.slice(1));
+}
