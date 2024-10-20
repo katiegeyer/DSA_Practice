bb74b2793ca627b285c736098dcf4a698a43c347
Katie Geyer <keg233@nau.edu>
2024-10-19 20:33:02 -0700

    duplicate

--- Practice.js
+++ Practice.js
@@ -2537,3 +2537,7 @@ function sumArray(arr) {
 function capitalizeWords(str) {
     return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
 }
+
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
